ac7ef0e63050ec4041b6e6dffa5c7c4dbfc567f0
Felix Staudigl <felix.staudigl@gmx.de>
2020-04-04 13:15:38 +0200

    another attempt ...

--- .travis/git_push.sh
+++ .travis/git_push.sh
@@ -6,13 +6,13 @@ setup_git() {
 }
 
 commit_website_files() {
-  git checkout -b master
-  git add bin/data_final.csv bin/compare_plot.png
+  git checkout master
+  git add bin/data_final.csv compare_plot.png
   git commit --message "Travis build: $TRAVIS_BUILD_NUMBER - Daily Data Update"
 }
 
 upload_files() {
-  git remote add origin https://${GITHUB_AUTH_TOKEN}@github.com/flombe/covid19_GER_RKI_datadifference.git > /dev/null 2>&1
+  git remote add origin https://flombe:${GITHUB_AUTH_TOKEN}@github.com/flombe/covid19_GER_RKI_datadifference.git > /dev/null 2>&1
   git push --quiet --set-upstream origin master
 }
 
