b78bdd5ba1c6788c931b6d52e759765f73d327cc
Felix Staudigl <felix.staudigl@gmx.de>
2020-04-04 12:24:46 +0200

    added travis git functionality

---
+++ .travis/git_push.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+setup_git() {
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis CI"
+}
+
+commit_website_files() {
+  git checkout -b master
+  git add bin/data_JHU.csv bin/data_RKI.csv bin/data_final.csv bin/compare_plot.png
+  git commit --message "Travis build: $TRAVIS_BUILD_NUMBER - Daily Data Update"
+}
+
+upload_files() {
+  git remote add origin https://${GITHUB_AUTH_TOKEN}@github.com/flombe/covid19_GER_RKI_datadifference.git > /dev/null 2>&1
+  git push --quiet --set-upstream origin master
+}
+
+echo "Setup Git..."
+setup_git
+echo "Add & Commit..."
+commit_website_files
+echo "Push..."
+upload_files
+echo "DONE.!"
